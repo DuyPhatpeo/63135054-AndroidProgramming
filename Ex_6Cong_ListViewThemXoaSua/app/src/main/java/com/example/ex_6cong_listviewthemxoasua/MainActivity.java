0f29efa9005cec2baf38220007ff74e81ad8ee85
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-03-11 11:14:59 +0700

    Chỉnh sửa hoàn thiện

--- Ex_6Cong_ListViewThemXoaSua/app/src/main/java/com/example/ex_6cong_listviewthemxoasua/MainActivity.java
+++ Ex_6Cong_ListViewThemXoaSua/app/src/main/java/com/example/ex_6cong_listviewthemxoasua/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,11 +19,21 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> dsTenTinhThanhVn; // Khai báo
+    Button btnThem, btnCapNhat, btnXoa;
+    EditText edtTinhThanh;
+    ListView lvTinhThanh;
+    int vitri = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        lvTinhThanh = (ListView)findViewById(R.id.lvDanhTT);
+        btnThem = (Button)findViewById(R.id.btnThem);
+        btnCapNhat = (Button)findViewById(R.id.btnCapNhat);
+        btnXoa = (Button)findViewById(R.id.btnXoa);
+        edtTinhThanh = (EditText)findViewById(R.id.edtTinhThanh);
 
         // Hiện thị dữ liệu của list view
         // B1: cần có dữ liệu
@@ -38,31 +50,40 @@ public class MainActivity extends AppCompatActivity {
         // B2: Tạo adapter
         ArrayAdapter<String> adapterTinhThanh;
         adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dsTenTinhThanhVn);
+        lvTinhThanh.setAdapter(adapterTinhThanh);
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tinhThanh = edtTinhThanh.getText().toString();
+                dsTenTinhThanhVn.add(tinhThanh);
+                adapterTinhThanh.notifyDataSetChanged();
+            }
+        });
 
-        // B3: Gắn vào điều khiển hiện thị ListView
-        // 3.1: tìm
-        ListView lvTenTinhThanh = findViewById(R.id.lvDanhTT);
-        // 3.2: Gắn
-        lvTenTinhThanh.setAdapter(adapterTinhThanh);
-        // 3.3: Lắng nghe và xử lý sự kiện user tương tác
-        // gắn bộ lắng nghe vào
-        lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXl);
+        // Xử lý btn cập nhật
+        lvTinhThanh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                edtTinhThanh.setText(dsTenTinhThanhVn.get(i));
+                vitri = i;
+            }
+        });
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dsTenTinhThanhVn.set(vitri,edtTinhThanh.getText().toString());
+                adapterTinhThanh.notifyDataSetChanged();
+            }
+        });
+
+        // Xử lý btn xoá
+        btnXoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dsTenTinhThanhVn.remove(vitri);
+                adapterTinhThanh.notifyDataSetChanged();
+            }
+        });
 
     }
-    // Tạo bộ lắng nghe và xử lý cho sự kiện OnItemClick, đặt vào một biến
-    AdapterView.OnItemClickListener BoLangNghevaXl = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-            // Code xử lý ở đây
-            // i là vị trí phần tử vừa click
-            // ví dụ xử lý ở đây, là hiển thị lên màn hình một thông báo nhanh
-
-            // về vị trí của phần tử vừa chọn
-            //Toast.makeText(MainActivity.this,"Bạn vừa chọn: " + String.valueOf(i),Toast.LENGTH_LONG).show();
-
-            // về giá trị của phần tử thứ i
-            String strTenTinhChon = dsTenTinhThanhVn .get(i);
-            Toast.makeText(MainActivity.this,"Bạn vùa chọn: " + strTenTinhChon,Toast.LENGTH_LONG).show();
-        }
-    };
 }
